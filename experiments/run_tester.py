8847b39b0d464b049f7b917d8405c6447ad31aa8
chenda duan <chenda@ucla.edu>
2023-06-08 13:29:24 -0700

    fix print result for torch GPU

--- experiments/run_tester.py
+++ experiments/run_tester.py
@@ -4,7 +4,7 @@ import numpy as np
 from basic_tester import *
 from needle_tester import *
 from torch_tester import *
-from IPython import embed
+# from IPython import embed
 
 def test_matmul(tester, m, n, p, A, B):
     data_func = lambda :tester._matmul_data(m, n, p, A, B)
@@ -15,10 +15,10 @@ def test_matmul(tester, m, n, p, A, B):
 if __name__ == "__main__":
     MATMUL_DIMS = [(128, 128, 128), (64, 4096, 1024)]
     tester_list = [
-        # NeedleCPUTester(),
+        NeedleCPUTester(),
         TorchCPUTester(),
         NeedleM1Tester(),
-        # TorchM1Tester(),
+        TorchM1Tester(),
         # NeedleCUDATester(),
         # TorchCUDATester(),
     ]
@@ -30,7 +30,12 @@ if __name__ == "__main__":
         res = True
         base_key = list(result_dict.keys())[0]
         for ele in result_dict.keys():
-            if not np.allclose(result_dict[base_key]["matmul"].numpy(), result_dict[ele]["matmul"].numpy(), rtol=1e-3, atol=1e-3):
+            print(ele)
+            if ele == "torch_m1_tester":
+                if not np.allclose(result_dict[base_key]["matmul"].numpy(), result_dict[ele]["matmul"].cpu().numpy(), rtol=1e-3, atol=1e-3):
+                    res = False
+                    break
+            elif not np.allclose(result_dict[base_key]["matmul"].numpy(), result_dict[ele]["matmul"].numpy(), rtol=1e-3, atol=1e-3):
                 res = False
                 break
         print("Are all values similar in dictionary? : " + str(res))
